e1ef3523a274b215678b28773f6ab4cf71ddfde8
maxgerashenko <mgerashchenko@gmail.com>
2021-11-04 22:16:52 -0400

    Add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDl9RF_osari1kfPwB_7P7577eHfGC7Vvw',
+  authDomain: 'maxgerashenkocom.firebaseapp.com',
+  projectId: 'maxgerashenkocom',
+  storageBucket: 'maxgerashenkocom.appspot.com',
+  messagingSenderId: '392261338644',
+  appId: '1:392261338644:web:743d92947ce232d96e0901',
+  measurementId: 'G-17WSZ4T33M',
+};
+
+const app = initializeApp(firebaseConfig);
